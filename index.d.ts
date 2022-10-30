85468e0859742ea0e71470ab0c3ffff8de481698
alexanderpo <alexander.pozhidaev@gmail.com>
2022-10-30 16:32:26 +0100

    fix: type declare

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,10 @@
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      ["session-expiration-popup"]: {
-        ["session-check-url"]?: string;
-        ["interval"]?: string | number;
-        ["force-show"]?: string | boolean;
-        ["clear-cookies"]?: string | boolean;
-      };
-    }
+declare namespace JSX {
+  interface IntrinsicElements {
+    ["session-expiration-popup"]: {
+      ["session-check-url"]?: string;
+      interval?: string | number;
+      ["force-show"]?: "true" | "false" | boolean;
+      ["clear-cookies"]?: "true" | "false" | boolean;
+    };
   }
 }
